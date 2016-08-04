2bd0a4fb028efff3cafb9292bd785a1fc72f8ea0
thucnhi2411 <let@lafayette.edu>
2016-08-04 23:01:23 +0700

    Add code

--- ImageGallery/ViewController.swift
+++ ImageGallery/ViewController.swift
@@ -10,15 +10,54 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageGallery: UIImageView!
+    var i: Int = 0
+    let gallery : [UIImage] = [
+        UIImage(named: "backgroundprofile.png")!,
+        UIImage(named: "backgroundedu.png")!,
+        UIImage(named: "backgroundexp.png")!,
+        UIImage(named: "backgroundaward.png")!,
+        UIImage(named: "backgroundskill")!
+    ]
     override func viewDidLoad() {
+        imageGallery.image = gallery[0]
+        let right = UISwipeGestureRecognizer(target: self, action: #selector(ViewController.rightSwiped(_:)) )
+        right.direction = UISwipeGestureRecognizerDirection.Right
+        self.view.addGestureRecognizer(right)
+        
+        
+        let left = UISwipeGestureRecognizer(target: self, action: #selector(ViewController.leftSwiped(_:)) )
+        left.direction = UISwipeGestureRecognizerDirection.Left
+        self.view.addGestureRecognizer(left)
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    }
+ 
+    func rightSwiped(recognizer: UISwipeGestureRecognizer){
+        print("Right Swiped")
+        if i < gallery.count && i > 0 {
+            i = i - 1
+            imageGallery.image = gallery[i]
+        }
+        
+
+    }
+    
+    
+    func leftSwiped(recognizer: UISwipeGestureRecognizer)
+    {
+        print("Left Swiped")
+        var a = gallery.count - 1
+        if i < a {
+            i = i + 1
+            imageGallery.image = gallery[i]
+            
+            
+        }
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+   
+    
 
 
 }
